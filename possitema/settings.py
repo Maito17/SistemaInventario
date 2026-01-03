f043eb720478864f4e76c102a1e97745f8034e96
Maito17 <149741599+Maito17@users.noreply.github.com>
2026-01-02 23:57:11 -0500

    Update ALLOWED_HOSTS for production settings

--- possitema/settings.py
+++ possitema/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kddezpmo^=m)+15*%wut@cgw(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.up.railway.app').split(',')
+ALLOWED_HOSTS = ['poreyctopersonal-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
