4281bb80e2ad5635e6f357a35a83cc204531b097
Maito17 <149741599+Maito17@users.noreply.github.com>
2026-02-20 22:02:33 -0500

    Comment out SuscripcionActivaRequiredMiddleware

--- possitema/settings.py
+++ possitema/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'possitema.middleware.SuscripcionActivaRequiredMiddleware',
+    #'possitema.middleware.SuscripcionActivaRequiredMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
