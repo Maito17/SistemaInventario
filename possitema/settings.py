0b25069a9ff9ec76550f99ef25ada09487259bd8
Maito17 <149741599+Maito17@users.noreply.github.com>
2026-02-20 21:50:18 -0500

    Add mysql.connector.django import to settings

--- possitema/settings.py
+++ possitema/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import dj_database_url
+import mysql.connector.django
 
 from pathlib import Path
 import os
